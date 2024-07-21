1ee7fb45aa36eafe6752217af6a9fb92a171e1e2
Gary Fan <tomfan419@gmail.com>
2024-07-22 01:39:28 +0800

    Update lifecycle

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
 }
 
